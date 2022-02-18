f351c679d00203cde4a37cd53610ad4d789669d8
Valentina Cupać <valentina.cupac@optivem.com>
2022-02-18 21:27:45 +0100

    HelloWorld - Refactoring

--- src/main/java/helloworld/HelloWorldRunner.java
+++ src/main/java/helloworld/HelloWorldRunner.java
@@ -1,8 +1,9 @@
 package helloworld;
 
 public class HelloWorldRunner {
+    private static String HELLO_WORLD = "Hello world!";
 
     public String run() {
-        return "Hello world!";
+        return HELLO_WORLD;
     }
 }
