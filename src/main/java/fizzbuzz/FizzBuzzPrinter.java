402336545f814fd28e50c438c2cb91d824801dab
Valentina Cupać <valentina.cupac@optivem.com>
2022-02-18 21:39:02 +0100

    FizzBuzz - Refactoring

--- src/main/java/fizzbuzz/FizzBuzzPrinter.java
+++ src/main/java/fizzbuzz/FizzBuzzPrinter.java
@@ -1,6 +1,9 @@
 package fizzbuzz;
 
 public class FizzBuzzPrinter {
+    private static int FIZZ_MOD = 3;
+    private static String FIZZ_LINE = "Fizz";
+
     private Printer printer;
 
     public FizzBuzzPrinter(Printer printer) {
@@ -9,12 +12,22 @@ public class FizzBuzzPrinter {
 
     public void print(int start, int end) {
         for(int i = start; i <= end; i++) {
-            if(i % 3 == 0) {
-                printer.print("Fizz");
-            } else {
-                var line = Integer.toString(i);
-                printer.print(line);
-            }
+            String line = getLine(i);
+            printer.print(line);
         }
+    }
+
+    private String getLine(int i) {
+        var isFizz = isDivisibleBy(i, FIZZ_MOD);
+
+        if(isFizz) {
+            return FIZZ_LINE;
+        } else {
+            return Integer.toString(i);
+        }
+    }
+
+    private boolean isDivisibleBy(int i, int mod) {
+        return i % mod == 0;
     }
 }
